54dc19ed46f8de337dcb6608205b2cf100a72246
hebamaloalain <96662273+hebamaloalain@users.noreply.github.com>
2022-04-03 16:31:21 +0300

    Add files via upload

---
+++ about.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+include ("partials/head.php");
+?>
+
+<body class="animsition">
+	<?php
+	include ("partials/header.php");
+	?>
+
+
+	<!-- Title page -->
+	<section class="bg-img1 txt-center p-lr-15 p-tb-92" style="background-image: url('images/bg-02.jpg');">
+		<h2 class="ltext-105 cl0 txt-center">
+			About
+		</h2>
+	</section>	
+
+
+	<!-- Content page -->
+	<section class="bg0 p-t-75 p-b-120">
+		<div class="container">
+			<div class="row p-b-148">
+				<div class="col-md-7 col-lg-8">
+					<div class="p-t-7 p-r-85 p-r-15-lg p-r-0-md">
+						<h3 class="mtext-111 cl2 p-b-16">
+							Our Story
+						</h3>
+
+						<p class="stext-113 cl6 p-b-26">
+							We started this company to share our ideas of new clothes and privide you with the best garments possible. enjoy
+						</p>
+
+						<p class="stext-113 cl6 p-b-26">
+						Any questions? Don't hesitate to contact us at hassan.abunaser10@gmail.com  or call us on 00962770128765
+						</p>
+					</div>
+				</div>
+
+				<div class="col-11 col-md-5 col-lg-4 m-lr-auto">
+					<div class="how-bor1 ">
+						<div class="hov-img0">
+							<img src="images/blog-02.jpg" alt="IMG">
+						</div>
+					</div>
+				</div>
+			</div>
+	
+		</div>
+	</section>	
+	
+		
+
+	<!-- Footer -->
+<?php 
+include ("partials/footer.php");
+?>
+</body>
+</html>
